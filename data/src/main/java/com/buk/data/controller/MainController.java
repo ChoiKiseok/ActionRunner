ad6d74e662bdd5674f917ee6046a679b31840910
ChoiKiseok <42999545+ChoiKiseok@users.noreply.github.com>
2022-06-15 15:00:43 +0900

    Test Log Add

--- data/src/main/java/com/buk/data/controller/MainController.java
+++ data/src/main/java/com/buk/data/controller/MainController.java
@@ -3,12 +3,15 @@ package com.buk.data.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Controller
+@Slf4j
 public class MainController {
   
   @GetMapping("/test")
   public String testPage() {
-    
+    log.debug("======= Test Page =====");
     return "index.html";
   }
 }
