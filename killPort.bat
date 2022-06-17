525e2c9fae884c0994ce2b6335aa0d5d11000bfa
ChoiKiseok <42999545+ChoiKiseok@users.noreply.github.com>
2022-06-17 10:21:02 +0900

    ...

--- killPort.bat
+++ killPort.bat
@@ -1,4 +1,5 @@
 @ECHO OFF
 SET killport=8080
 for /f "tokens=5" %%p in ('netstat -aon ^| find /i "listening" ^| find "%killport%"') do taskkill /F /PID %%p
+pause
 START /b java -jar target/data.jar >> data.log &
