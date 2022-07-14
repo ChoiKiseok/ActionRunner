1f86167f67dd97eff1a3e7890064679735374076
cks1649 <cks1649@naver.com>
2022-07-14 16:39:38 +0900

    spring security update

--- src/main/java/com/buk/admin/controller/AdminController.java
+++ src/main/java/com/buk/admin/controller/AdminController.java
@@ -3,11 +3,23 @@ package com.buk.admin.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.buk.user.service.UserService;
+
+import lombok.RequiredArgsConstructor;
+
 @Controller
+@RequiredArgsConstructor
 public class AdminController {
+    private final UserService userService;
     
     @GetMapping("/admin")
     public String admin() {
         return "admin/admin";
+    }
+
+    @GetMapping("/login")
+    public String loginPage() {
+        userService.getPasswordEncode();
+        return "/admin/login";
     }
 }
--- src/main/java/com/buk/config/SecrurityConfig.java
+++ src/main/java/com/buk/config/SecrurityConfig.java
@@ -1,40 +1,58 @@
-// package com.buk.config;
+package com.buk.config;
 
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-// import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-// import org.springframework.security.web.SecurityFilterChain;
-// import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import javax.sql.DataSource;
 
-// @Configuration
-// public class SecrurityConfig {
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.boot.autoconfigure.security.ConditionalOnDefaultWebSecurity;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-//   @Bean
-//   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//     http.csrf().disable()
-//         .authorizeRequests()
-//         .antMatchers("/login").permitAll()
-//         .antMatchers("/admin/**").authenticated()
-//         .and()
-//         .formLogin()
-//         .usernameParameter("userId")
-//         .passwordParameter("userPw")
-//         .loginPage("/login")
-//         .loginProcessingUrl("/login")
-//         .defaultSuccessUrl("/admin/")
-//         .permitAll()
-//         .and()
-//         .logout()
-//         .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-//         .logoutSuccessUrl("/login")
-//         .invalidateHttpSession(true);
+import lombok.RequiredArgsConstructor;
 
-//     return http.build();
-//   }
+@Configuration
+@RequiredArgsConstructor
+@ConditionalOnDefaultWebSecurity
+@ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.SERVLET)
+public class SecrurityConfig {
+    private final DataSource dataSource;
 
-//   @Bean
-//   public WebSecurityCustomizer webSecurityCustomizer() {
-//     return (web) -> web.ignoring().antMatchers("/css/**", "/js/**", "/img/**");
-//   }
-// }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+          .authorizeRequests()
+          .antMatchers("/login").permitAll()
+          .antMatchers("/admin/**").authenticated()
+          .and()
+        .formLogin()
+          .usernameParameter("userId")
+          .passwordParameter("userPw")
+          .loginPage("/login")
+          .loginProcessingUrl("/login")
+          .defaultSuccessUrl("/admin/")
+          .permitAll()
+          .and()
+        .logout()
+          .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+          .logoutSuccessUrl("/login")
+          .invalidateHttpSession(true);
+
+        return http.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().antMatchers("/css/**", "/js/**", "/img/**");
+    }
+    
+}
